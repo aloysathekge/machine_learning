5d24c199d0570071c90fe03c7ea4c2be7513a063
aloysathekge <sathekgealoy@gmail.com>
2026-01-09 07:06:34 +0200

    Add foundational numpy array operations in neural_network/foundation.py

---
+++ neural_network/foundation.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+a=np.array([[2,3,5],[0,3,-3]])
+b=np.array([[1,2,5],[0,1,-1]])
+
+ab=a+b
+print(ab)
+# print(a.shape)
+# print(a.ndim)
+# print(a.size)
+# print(a.itemsize)
+# print(a.dtype)
+
+
+
