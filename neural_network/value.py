0b93ba113ff95e25d9f384a2b2c2c8c9f738a514
aloysathekge <sathekgealoy@gmail.com>
2026-01-14 06:32:10 +0200

    add and multiply

--- neural_network/value.py
+++ neural_network/value.py
@@ -1,2 +1,32 @@
+from ast import Lambda
+from turtle import backward
+
+
 class Value:
-   
+   def __init__(self,data,_children=(),_op=''):
+    self.data=data
+    self.grad=0.0
+    self._prev=set(_children)
+    self._op=_op
+    self._backward=lambda: None
+
+
+    def __add__(self,other):
+        out=Value(self.data +other.data,(self,data) , '+')
+        
+        def _backward():
+            # Gradient flows equally to both inputs
+            self.grad+=1.0 *out.grad
+            other.grad +=1.0 * out.grad
+        out._backward=_backward
+        return out
+
+    def __mul__(self,other):
+        out=Value(self.data * other.data, (self,other), '*')
+
+        def _backward():
+            # Chain rule d(a*b)/da=b,d(a*b)/db=a
+            self.grad +=other.data *out.grad
+            other.grad +=self.data * out.grad
+        out._backward=_backward
+        return out
