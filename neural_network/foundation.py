77cc1724d27edc7a1cd8f0d22c01654c719f5022
aloysathekge <sathekgealoy@gmail.com>
2026-01-14 06:32:27 +0200

    foundation

--- neural_network/foundation.py
+++ neural_network/foundation.py
@@ -1,10 +1,12 @@
 import numpy as np
 
 a=np.array([[2,3,5],[0,3,-3]])
-b=np.array([[1,2,5],[0,1,-1]])
+b=np.array([[1,2],[0,1],[2,4]])
 
-ab=a+b
+ab=np.dot(a,b)
+ab_flat=ab.flatten()
 print(ab)
+print(ab_flat)
 # print(a.shape)
 # print(a.ndim)
 # print(a.size)
