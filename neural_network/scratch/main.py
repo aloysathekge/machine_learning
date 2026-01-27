8185a08cc18ae536751b5e120267b675549c5854
aloysathekge <sathekgealoy@gmail.com>
2026-01-27 11:32:19 +0200

    feat: Implement a main script for training and testing an MLP to approximate the y=x^2 function.

--- neural_network/scratch/main.py
+++ neural_network/scratch/main.py
@@ -3,15 +3,28 @@ from value import Value
 from engine import SGD, MSELoss, Module, Neuron, Layer, MLP
 
 
-xs = [[Value(x)] for x in [-2.0, -1.0, 0.0, 1.0, 2.0]]
-ys = [Value(x**2) for x in [-2.0, -1.0, 0.0, 1.0, 2.0]]
+xs = [
+    [Value(-2.0)], [Value(-1.8)], [Value(-1.6)], [Value(-1.4)], [Value(-1.2)],
+    [Value(-1.0)], [Value(-0.8)], [Value(-0.6)], [Value(-0.4)], [Value(-0.2)],
+    [Value(0.0)],
+    [Value(0.2)], [Value(0.4)], [Value(0.6)], [Value(0.8)], [Value(1.0)],
+    [Value(1.2)], [Value(1.4)], [Value(1.6)], [Value(1.8)], [Value(2.0)]
+]
 
-mlp=MLP(1,[4,1])
+ys = [
+    Value(4.00), Value(3.24), Value(2.56), Value(1.96), Value(1.44),
+    Value(1.00), Value(0.64), Value(0.36), Value(0.16), Value(0.04),
+    Value(0.00),
+    Value(0.04), Value(0.16), Value(0.36), Value(0.64), Value(1.00),
+    Value(1.44), Value(1.96), Value(2.56), Value(3.24), Value(4.00)
+]
 
-optimizer = SGD(mlp.parameters(), lr=0.001)
+mlp=MLP(1,[10,1])
+
+optimizer = SGD(mlp.parameters(), lr=0.01)
 criterion =MSELoss()
 
-for epoch in range(10000):
+for epoch in range(5000):
     optimizer.zero_grad()
     #forward
     preds=[mlp(x) for x in xs]
